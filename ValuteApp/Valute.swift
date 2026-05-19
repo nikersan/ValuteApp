a3f8316174ce8be1a5992d79927e2522afc929a3
Nikita Chuklov <chuklovns91@mail.ru>
2026-05-19 19:24:00 +0300

    clear code

--- ValuteApp/Valute.swift
+++ ValuteApp/Valute.swift
@@ -8,13 +8,6 @@
 import Foundation
 import SwiftData
 
-// Valute
-// name
-// current course
-// prev course
-// num code
-// char code
-
 //@Model
 class Rate: Decodable {
     enum CodingKeys: CodingKey {
@@ -102,7 +95,6 @@ struct Response: Decodable {
 }
 
 struct ValuteDetail: Decodable {
-   
     enum CodingKeys: String, CodingKey {
         case id = "ID"
         case numCode = "NumCode"
@@ -119,35 +111,3 @@ struct ValuteDetail: Decodable {
     let value: Double
     let previous: Double
 }
-
-
-
-//"ID": "R01010",
-//      "NumCode": "036",
-//      "CharCode": "AUD",
-//      "Nominal": 1,
-//      "Name": "Австралийский доллар",
-//      "Value": 52.3593,
-//      "Previous": 53.0839
-
-
-// valuteDescription
-// valuteDetail
-
-//"Cur_ID": 1,
-//    "Cur_ParentID": 1,
-//    "Cur_Code": "008",
-//    "Cur_Abbreviation": "ALL",
-//    "Cur_Name": "Албанский лек",
-//    "Cur_Name_Bel": "Албанскі лек",
-//    "Cur_Name_Eng": "Albanian Lek",
-//    "Cur_QuotName": "1 Албанский лек",
-//    "Cur_QuotName_Bel": "1 Албанскі лек",
-//    "Cur_QuotName_Eng": "1 Albanian Lek",
-//    "Cur_NameMulti": "",
-//    "Cur_Name_BelMulti": "",
-//    "Cur_Name_EngMulti": "",
-//    "Cur_Scale": 1,
-//    "Cur_Periodicity": 1,
-//    "Cur_DateStart": "1991-01-01T00:00:00",
-//    "Cur_DateEnd": "2007-11-30T00:00:00"
